1e9362ce0639e89f04c61091d59745452a7e94e2
Unknown <justinkinhanghai@gmail.com>
2017-04-28 19:48:16 -0400

    Imported PIL

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from backend import *
 import tkinter
-
+from PIL import ImageTk, Image
 class GUI:
     def __init__(self, backendObject):
         self.backend = backendObject
@@ -9,5 +9,11 @@ class GUI:
     def run(self):
         root = tkinter.Tk();
         root.geometry('1024x640')
+        
+        #Load First image automatically
+        path = "image1.jpg"
+        img = ImageTk.PhotoImage(Image.open(path))
+        #Add hotkeys for labelling, undoing, next and previous
+        #Re-render after all commands
         pass
         
