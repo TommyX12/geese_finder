588bb2da3cbdaa23c041180cf24433f74d5fadf8
TommyX12 <tommyx058@gmail.com>
2017-04-30 01:58:49 -0400

    Added ability to manually geo-locate items

--- backend.py
+++ backend.py
@@ -6,47 +6,68 @@ from util import *
 
 TODOs:
 - display image name and other stats on bottom of screen
-- allow indicator of previously marked geese somehow
+- extensive testing
+- research on features of each type of goose
+
+done:
 - allow zooming of image at point
-- test GPS and lens correction
+- count number of geese of each type
+- improve geese type identification
+
+later:
+- allow indicator of previously marked geese somehow
+- mark gps when marking on picture
+- interpolate using reference points
+- click on map geese to jump to the picture with corresponding marking
+- print random shit
 
 """
 
 class Goose:
-    def __init__(self, x, y, type):
-        self.x = x
-        self.y = y
+    def type_is_goose(type):
+        return type >= '0' and type <= '9'
+
+    def __init__(self, x, y, lat, lon, type):
+        self.x    = x
+        self.y    = y
+        self.lat  = lat
+        self.lon  = lon
         self.type = type
     
     def get_data(self, image_object):
-        corrected = correct_point(self.x, self.y, image_object.latitude, image_object.longitude, image_object.heading)
+        #  corrected = correct_point(self.x, self.y, image_object.latitude, image_object.longitude, image_object.heading)
         return {
-            'x':             self.x,
-            'y':             self.y,
-            'type':          self.type,
-            'id':            image_object.id,
-            'cam-yaw':       image_object.yaw,
-            'cam-pitch':     image_object.pitch,
-            'cam-roll':      image_object.roll,
-            'cam-altitude':  image_object.altitude,
-            'image-name':    image_object.path,
-            'image-time':    image_object.time,
-            'cam-longitude': image_object.longitude,
-            'cam-latitude':  image_object.latitude,
-            'cam-heading':   image_object.heading,
-            'corrected-x':   corrected[0],
-            'corrected-y':   corrected[1],
+            'x':          self.x,
+            'y':          self.y,
+            'latitude':   self.lat,
+            'longitude':  self.lon,
+            'type':       self.type,
+            'id':         image_object.id,
+            #  'cam-yaw':       image_object.yaw,
+            #  'cam-pitch':     image_object.pitch,
+            #  'cam-roll':      image_object.roll,
+            #  'cam-altitude':  image_object.altitude,
+            'image-name': image_object.path,
+            #  'image-time': image_object.time,
+            #  'cam-latitude':  image_object.latitude,
+            #  'cam-longitude': image_object.longitude,
+            #  'cam-heading':   image_object.heading,
+            #  'corrected-x':   corrected[0],
+            #  'corrected-y':   corrected[1],
         }
     
     def from_data(data):
         return Goose(
             float(data['x']),
             float(data['y']),
+            float(data['latitude']),
+            float(data['longitude']),
             str(data['type']),
         )
 
 class ImageObject:
     def __init__(self,
+            backend   = None,
             id        = 0,
             geese     = [],
             path      = '',
@@ -59,6 +80,7 @@ class ImageObject:
             heading   = 0,
             time      = 0,
             ):
+        self.backend   = backend
         self.id        = id
         self.geese     = geese
         self.path      = path
@@ -71,8 +93,8 @@ class ImageObject:
         self.heading   = heading
         self.time      = time
     
-    def add_goose(self, x, y, type):
-        self.geese.append(Goose(x, y, type))
+    def add_goose(self, x, y, lat, lon, type):
+        self.geese.append(Goose(x, y, lat, lon, type))
     
     def pop_goose(self):
         if len(self.geese) > 0:
@@ -81,7 +103,9 @@ class ImageObject:
     def get_geese(self):
         return self.geese
 
-GPS_DATA_FILENAME = 'gps_data.csv'
+#  GPS_DATA_FILENAME = 'gps_data.csv'
+MAP_FILENAME = 'geo_info/map.png'
+CORNERS_FILENAME = 'geo_info/corners.csv'
 GEESE_RAW_FILENAME = 'output/geese_raw.csv'
 NESTS_RAW_FILENAME = 'output/nests_raw.csv'
 COUNTS_FILENAME = 'output/counts.csv'
@@ -114,34 +138,36 @@ class Backend:
         print('back end initialized.')
     
     def load(self):
-        gps_data    = csv_to_dicts(read_file(GPS_DATA_FILENAME))
+        #  gps_data    = csv_to_dicts(read_file(GPS_DATA_FILENAME))
         geese_raw   = csv_to_dicts(read_file(GEESE_RAW_FILENAME))
+        corners     = csv_to_dicts(read_file(CORNERS_FILENAME))
         image_paths = list_dir('images/*.jpg')
         
-        geese = {}
+        geese_id = {}
         for goose_raw in geese_raw:
             id = int(goose_raw['id'])
-            if id not in geese:
-                geese[id] = []
+            if id not in geese_id:
+                geese_id[id] = []
                 
-            geese[id].append(Goose.from_data(goose_raw))
+            geese_id[id].append(Goose.from_data(goose_raw))
         
         ids = []
         for path in image_paths:
             id = image_path_to_id(path)
-            gps_data_entry = gps_data[id - 1]
+            #  gps_data_entry = gps_data[id - 1]
             image_object = ImageObject(
+                self,
                 id,
-                geese[id] if id in geese else [],
+                geese_id[id] if id in geese_id else [],
                 path      = path,
                 yaw       = 0,
                 pitch     = 0,
                 roll      = 0,
                 altitude  = 0,
-                longitude = semicircles_to_degrees(float(gps_data_entry['position_long_value'])),
-                latitude  = semicircles_to_degrees(float(gps_data_entry['position_lat_value'])),
-                heading   = float(gps_data_entry['heading_value']),
-                time      = gps_data_entry['readable_time_value'],
+                #  longitude = semicircles_to_degrees(float(gps_data_entry['position_long_value'])),
+                #  latitude  = semicircles_to_degrees(float(gps_data_entry['position_lat_value'])),
+                #  heading   = float(gps_data_entry['heading_value']),
+                #  time      = gps_data_entry['readable_time_value'],
             )
             
             ids.append(id)
@@ -151,6 +177,29 @@ class Backend:
         
         for id in ids:
             self.image_objects_list.append(self.image_objects_dict[id])
+        
+        self.map = ImageObject(
+            path = MAP_FILENAME,
+        )
+        
+        u1 = Vector(corners[0]['x'], corners[0]['y'])
+        u2 = Vector(corners[1]['x'], corners[1]['y'])
+        u3 = Vector(corners[2]['x'], corners[2]['y'])
+        v1 = Vector(corners[0]['lat'], corners[0]['lon'])
+        v2 = Vector(corners[1]['lat'], corners[1]['lon'])
+        v3 = Vector(corners[2]['lat'], corners[2]['lon'])
+        self.corners_u = u1
+        self.corners_v = v1
+        
+        a1 = u2.sub(u1)
+        a2 = u3.sub(u1)
+        b1 = v2.sub(v1)
+        b2 = v3.sub(v1)
+        
+        self.corners_mu = Matrix(b1, b2).mul_mat(Matrix(a1, a2).inverse())
+        self.corners_mv = Matrix(a1, a2).mul_mat(Matrix(b1, b2).inverse())
+        
+        # u = map, v = gps, mu = from map, mv = from gps
             
         print('back end loaded.')
     
@@ -160,26 +209,64 @@ class Backend:
     def get_image_object_by_id(self, id):
         return self.image_objects_dict[id]
     
+    def get_map(self):
+        return self.map
+    
+    def gps_to_map(self, gps):
+        #  tl = self.corners['top_left']
+        #  tr = self.corners['top_right']
+        #  bl = self.corners['bottom_left']
+        #  br = self.corners['bottom_right']
+        #  dx = tr.sub(tl)
+        #  dy = bl.sub(tl)
+        #  return solve_matrix_2d(dx.x, dy.x, dx.y, dy.y, gps.x - tl.x, gps.y - tl.y)
+        return self.corners_mv.mul_vec(gps.sub(self.corners_v)).add(self.corners_u)
+    
+    def map_to_gps(self, map):
+        #  tl = self.corners['top_left']
+        #  tr = self.corners['top_right']
+        #  bl = self.corners['bottom_left']
+        #  br = self.corners['bottom_right']
+        #  return tl.add(tr.sub(tl).mul(map.x)).add(bl.sub(tl).mul(map.y))
+        return self.corners_mu.mul_vec(map.sub(self.corners_u)).add(self.corners_v)
+    
     def save(self):
         dicts = []
         dicts_nests = []
         total_geese = 0
-        total_types = {}
+        types = {}
         for image_object in self.image_objects_list:
             dicts += [goose.get_data(image_object) for goose in image_object.get_geese()]
         
         for goose in dicts:
-            if goose['type'] == 'nest':
+            type = goose['type']
+            if type == 'nest':
                 dicts_nests.append(goose)
             
-            else:
-                total_geese += 1
-                total_types[goose['type']] = 1
+            total_geese += 1
+            if type not in types:
+                types[type] = 0
+                
+            types[type] += 1
         
-        counts = [{
-            'total-geese': total_geese,
-            'total-types': len(total_types),
-        }]
+        counts = [
+            {
+                'name': 'total-geese',
+                'value': total_geese,
+            },
+            {
+                'name': 'total-types',
+                'value': len(types),
+            },
+        ]
+        
+        for type in types:
+            counts.append(
+                {
+                    'name': 'count-' + type,
+                    'value': types[type],
+                }
+            )
         
         write_file(GEESE_RAW_FILENAME, dicts_to_csv(dicts))
         write_file(NESTS_RAW_FILENAME, dicts_to_csv(dicts_nests))
@@ -190,4 +277,30 @@ if __name__ == '__main__':
     backend.load()
     # image_object = backend.get_image_object_by_id(1)
     # image_object.add_goose(0.5, 0.5, 'bad goose')
+    u1 = backend.gps_to_map(Vector(48.507887717796294,-71.63648124734259))
+    u2 = backend.gps_to_map(Vector(48.508937414837504,-71.6501081183125))
+    u3 = backend.gps_to_map(Vector(48.51413814770878,-71.64029885586213))
+    v1 = Vector(48.50789, -71.63646)
+    v2 = Vector(48.50901, -71.65012)
+    v3 = Vector(48.5143, -71.6402)
+    
+    a1 = u2.sub(u1)
+    a2 = u3.sub(u1)
+    b1 = v2.sub(v1)
+    b2 = v3.sub(v1)
+    
+    c1 = solve_matrix_2d(Matrix(a1, a2), Vector(1, 0))
+    c2 = solve_matrix_2d(Matrix(a1, a2), Vector(0, 1))
+    i1 = b1.mul(c1.x).add(b2.mul(c2.x))
+    i2 = b1.mul(c1.y).add(b2.mul(c2.y))
+    
+    def transform(u):
+        u = u.sub(u1)
+        return Vector(i1.x * u.x + i2.x * u.y, i1.y * u.x + i2.y * u.x).add(v1)
+    
+    #  print(transform(Vector(0, 0)))
+    #  print(transform(Vector(1, 0)))
+    #  print(transform(Vector(0, 1)))
+    #  print(transform(Vector(1, 1)))
+
     backend.save()
--- util.py
+++ util.py
@@ -71,3 +71,73 @@ MAX_SEMICIRCLES = 2**31
 
 def semicircles_to_degrees(semicircles):
     return semicircles * (180.0 / MAX_SEMICIRCLES)
+
+def clamp(num, l, r):
+    if num < l:
+        return l
+
+    if num > r:
+        return r
+    
+    return num
+
+def solve_matrix_2d(m, v):
+    #  idet = 1 / (a * d - b * c)
+    #  ia = d * idet
+    #  ib = -b * idet
+    #  ic = -c * idet
+    #  id = a * idet
+    #  return Vector(ia * e + ib * f, ic * e + id * f)
+    return m.inverse().mul_vec(v)
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+    
+    def add(self, v):
+        return Vector(self.x + v.x, self.y + v.y)
+    
+    def sub(self, v):
+        return Vector(self.x - v.x, self.y - v.y)
+
+    def mul(self, c):
+        return Vector(self.x * c, self.y * c)
+    
+    def mul2(self, x, y):
+        return Vector(self.x * x, self.y * y)
+    
+    def __str__(self):
+        return str(self.x) + ', ' + str(self.y)
+    
+    def to_tuple(self):
+        return (self.x, self.y)
+
+class Matrix:
+    def __init__(self, a, b, c=None, d=None):
+        if c == None and d == None:
+            self.a = a.x
+            self.b = b.x
+            self.c = a.y
+            self.d = b.y
+        
+        else:
+            self.a = a
+            self.b = b
+            self.c = c
+            self.d = d
+    
+    def mul(self, c):
+        return Matrix(self.a * c, self.b * c, self.c * c, self.d * c)
+    
+    def mul_vec(self, v):
+        return Vector(v.x * self.a + v.y * self.b, v.x * self.c + v.y * self.d)
+    
+    def inverse(self):
+        return Matrix(self.d, -self.b, -self.c, self.a).mul(1 / (self.a * self.d - self.b * self.c))
+    
+    def mul_mat(self, m):
+        return Matrix(self.mul_vec(Vector(m.a, m.c)), self.mul_vec(Vector(m.b, m.d)))
+
+if __name__ == '__main__':
+    pass
